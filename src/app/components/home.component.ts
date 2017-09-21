6a882e37b503ee673d79ca60f20bfc94c13f1e84
niralpokal <niralpokal@gmail.com>
2017-09-20 21:04:34 -0700

    Created home component and created background image and logo.

---
+++ src/app/components/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    styleUrls:['home.component.scss'],
+    template: `
+    <div class="parallex container overlay">
+        <div class="center pre-white">
+            <span class="padding"><img src="../../assets/logo.png"></span>
+        </div>
+    </div>
+    `
+})
+
+export class HomeComponent {
+
+}
