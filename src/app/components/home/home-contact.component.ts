cd1aad101345929ed1b52c71b0af6cd9fa973ed2
niralpokal <niralpokal@gmail.com>
2017-09-24 18:30:20 -0700

    Updated home contact component.

--- src/app/components/home/home-contact.component.ts
+++ src/app/components/home/home-contact.component.ts
@@ -11,14 +11,23 @@ import { Component } from '@angular/core';
     </div>
     <div class="flex-container">
         <div class="flex-item">
-            <p>Location:</p>
-            <img src="../../../assets/profile.jpg" >
+            <div class="link-container"> 
+                <p>Email: </p>
+                <a href="mailto:emma.cartwright.43@gmail.com">emma.cartwright.43@gmail.com</a> 
+            </div>
         </div>
         <div class="flex-item">
-            <p> Drop a dime here</p>
-            <input>
-
-        </div>    
+            <div class="link-container"> 
+                <p>Phone:</p>
+                <a href="tel://302.540.2575">(302) 540-2575</a>
+            </div>
+        </div> 
+        <div class="flex-item"> 
+            <div class="link-container"> 
+                <p>LinkedIn: </p>
+                <a href="https://linkedin.com/in/emmacartwright">linkedin.com/in/emmacartwright</a>
+            </div>
+        <div>  
     </div>
     `
 })
