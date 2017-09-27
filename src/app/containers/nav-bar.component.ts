00869dc75d7efd2a391e2da40438efca470deab5
niralpokal <niralpokal@gmail.com>
2017-09-26 21:03:13 -0700

    Added more temporary nav items.

--- src/app/containers/nav-bar.component.ts
+++ src/app/containers/nav-bar.component.ts
@@ -5,15 +5,19 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['nav-bar.component.scss'],
     template: `
     <div class="top">
-        <div [className]="class">
+        <div [className]="navBarClass">
             <a class="nav-item nav-button">Home</a>
+            <a class="nav-item nav-button">Portfolio</a>
+            <a class="nav-item nav-button">Instagram</a>
+            <a class="nav-item nav-button">Other</a>
         </div>
     </div>
     `
 })
 
 export class NavBarComponent implements OnInit {
-    class:string = 'bar';
+    navBarClass:string = 'bar';
+
     ngOnInit() {
         document.addEventListener('scroll', this.scroll, true); 
     }
@@ -24,8 +28,7 @@ export class NavBarComponent implements OnInit {
 
     scroll = (e):void => { 
        let top = document.documentElement.scrollTop || document.body.scrollTop;
-       if ( top >100) this.class = 'white-back nav-trans bar';
-       else this.class = 'bar'; 
-       console.log(this.class)
+       if (top > 200) this.navBarClass = 'white-back nav-trans bar';
+       else this.navBarClass = 'bar'; 
     }
 }
