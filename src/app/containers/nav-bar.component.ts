4ff24cdf120f9f45db3a02c2983528b063b69b83
niralpokal <niralpokal@gmail.com>
2017-09-28 20:40:13 -0700

    Added basic responive nav-bar

--- src/app/containers/nav-bar.component.ts
+++ src/app/containers/nav-bar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
     template: `
     <div class="top">
         <div [className]="navBarClass">
-            <a class="nav-item nav-button">Home</a>
-            <a class="nav-item nav-button">Portfolio</a>
-            <a class="nav-item nav-button">Instagram</a>
-            <a class="nav-item nav-button">Other</a>
+            <a class="nav-item nav-button hide-small-down">Home</a>
+            <a class="nav-item nav-button hide-small-down">Portfolio</a>
+            <a class="nav-item nav-button hide-small-down">Instagram</a>
+            <a class="nav-item nav-button hide-small-down">Other</a>
+            <a class="nav-item-block nav-button hide-med-up">Home</a>
+            <a class="nav-item-block nav-button hide-med-up">Portfolio</a>
+            <a class="nav-item-block nav-button hide-med-up">Instagram</a>
+            <a class="nav-item-block nav-button hide-med-up">Other</a>
         </div>
     </div>
     `
