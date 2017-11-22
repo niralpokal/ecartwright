cb458b39b18c7f2179eab833b58f02cbd931fb1f
Niral Pokal <niralpokal@gmail.com>
2017-11-21 21:13:26 -0800

    Updated navbar to propery hide the navbar items in a small viewpoint

--- src/app/containers/home.component.ts
+++ src/app/containers/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    styleUrls:['home.component.scss'],
+    styleUrls: ['home.component.scss'],
     template: `
     <div class="parallex container overlay">
         <div class="center white-space">
