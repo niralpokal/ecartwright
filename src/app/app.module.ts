db520eba079f8014f0cf656580f155a47f7fee26
Niral Pokal <niralpokal@gmail.com>
2017-11-22 21:33:28 -0800

    Added image service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './containers/home.component';
@@ -7,6 +8,7 @@ import { NavBarComponent } from './containers/nav-bar.component';
 import { HomeAboutMeComponent } from './components/home/home-about-me.component';
 import { HomePortfolioComponent } from './components/home/home-portfolio.component';
 import { HomeInstagramComponent } from './components/home/home-instagram.component';
+import { ImageService } from './services/image.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { HomeInstagramComponent } from './components/home/home-instagram.compone
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ImageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/image.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http/src/http';
+import { Response } from '@angular/http/src/static_response';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+const IMAGE_API = '/api/images';
+@Injectable()
+
+export class ImageService {
+    constructor (private http: Http) {}
+
+    getHomeImages() {
+        return this.http
+            .get(`${IMAGE_API}/home`)
+            .map((response: Response) => response.json());
+    }
+
+    getInstagramImages() {
+        return this.http
+            .get(`${IMAGE_API}/instagram`)
+            .map((response: Response) => response.json());
+    }
+
+    getPortfolioImages() {
+        return this.http
+            .get(`${IMAGE_API}/portfolio`)
+            .map((response: Response) => response.json());
+    }
+}
